b88cacabd0b0715097809c254c879aa2a9db1dfd
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-19 13:53:10 -0600

    feat(SignUpView): implement the SignUpView component along its route to the clerk sign-up layout

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SignInView from "@/views/SignInView.vue";
+import SignUpView from "@/views/SignUpView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/sign-in",
       name: "signin",
       component: SignInView,
+    },
+    {
+      path: "/sign-up",
+      name: "signup",
+      component: SignUpView,
     },
   ],
 });
---
+++ FrontEnd/src/views/SignUpView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { SignUp } from "vue-clerk";
+</script>
+
+<template>
+  <SignUp path="/sign-up" />
+</template>
