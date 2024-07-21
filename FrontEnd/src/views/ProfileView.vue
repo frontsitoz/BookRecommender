063f3288038b0bf7e4da3ac9c882cfb78e30dd46
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-21 01:27:47 -0600

    feat(ProfileView): implement the profile page container initial layout along the clerk username rendering

--- FrontEnd/src/views/ProfileView.vue
+++ FrontEnd/src/views/ProfileView.vue
@@ -1,14 +1,27 @@
 <script setup>
-import { useAuth } from "vue-clerk";
+import { useAuth, useUser } from "vue-clerk";
 import { useMiddleware } from "@/helpers/useMiddleware";
 
 const { isSignedIn } = useAuth();
+const { user } = useUser();
+
+const currentUser = user.value;
 
 useMiddleware(isSignedIn.value);
 </script>
 
 <template>
   <main
-    class="bg-gradient-to-br from-[#0A0C15] via-[#1D4938] to-green-700 w-full h-[calc(100vh-80px)]"
-  ></main>
+    class="flex justify-center items-center px-24 py-12 bg-gradient-to-br from-[#0A0C15] via-[#1D4938] to-green-700 w-full h-[calc(100vh-80px)]"
+  >
+    <div
+      class="flex flex-col py-10 gap-9 w-full h-full bg-[#0A0C15] rounded-full"
+    >
+      <div>
+        <p class="text-white text-6xl text-center">
+          {{ currentUser.username }}
+        </p>
+      </div>
+    </div>
+  </main>
 </template>
