227a02568451e17c7da38f2d3d51d3ca5ede5ed1
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-22 21:52:20 -0600

    feat(BookCard): implement the BookCard component initial layout along its initial structure

---
+++ FrontEnd/src/components/BookCard.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div class="w-52 h-52 mt-7"></div>
+</template>
--- FrontEnd/src/components/ProfileCategory.vue
+++ FrontEnd/src/components/ProfileCategory.vue
@@ -1,25 +1,17 @@
 <script setup>
 import { defineProps } from "vue";
+import BookCard from "./BookCard.vue";
 
 const props = defineProps(["title"]);
+
+const cards = [1, 2, 3, 4];
 </script>
 
 <template>
   <article class="flex flex-col w-full h-auto">
     <h1 class="text-white text-4xl font-castoro">{{ props.title }}:</h1>
     <div class="flex flex-wrap justify-between w-full h-auto">
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
-      <div class="w-52 h-52 mt-7"></div>
+      <BookCard v-for="(card, index) in cards" :key="index" />
     </div>
   </article>
 </template>
