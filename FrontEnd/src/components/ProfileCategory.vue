85b4fab3c73df041d497f0b8f6a08f6f3d8d52b8
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-22 21:46:17 -0600

    feat(ProfileCategory): implement the complet ProfileCategory component layout and props

--- FrontEnd/src/components/ProfileCategory.vue
+++ FrontEnd/src/components/ProfileCategory.vue
@@ -1,3 +1,25 @@
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
 
-<template></template>
+const props = defineProps(["title"]);
+</script>
+
+<template>
+  <article class="flex flex-col w-full h-auto">
+    <h1 class="text-white text-4xl font-castoro">{{ props.title }}:</h1>
+    <div class="flex flex-wrap justify-between w-full h-auto">
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+      <div class="w-52 h-52 mt-7"></div>
+    </div>
+  </article>
+</template>
