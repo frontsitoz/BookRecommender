1327a279c26342feac61e2551705f41e21c68d70
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-24 12:42:45 -0600

    feat(HomeLeftSidebar): implement the SidebarItem component within the main component

--- FrontEnd/src/components/home/HomeLeftSidebar.vue
+++ FrontEnd/src/components/home/HomeLeftSidebar.vue
@@ -1,78 +1,18 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import SidebarItem from "./SidebarItem.vue";
+
+// Importar las imágenes
+import starIcon from "@/assets/images/star-icon.svg";
+import flameIcon from "@/assets/images/flame-icon.svg";
+import categoryIcon from "@/assets/images/category-icon.svg";
 </script>
 
 <template>
   <section class="w-72 h-full px-7 py-10 bg-[#060814]">
     <div class="flex flex-col gap-12 w-full h-full pl-4">
-      <RouterLink
-        to="/"
-        class="flex justify-between text-white hover:text-[#b5b5b5]"
-      >
-        <div class="flex gap-2">
-          <p class="text-2xl font-castoro">New</p>
-          <img
-            src="@/assets/images/star-icon.svg"
-            alt="category_icon"
-            width="20"
-            height="20"
-            class="invert mr-10 mb-1"
-          />
-        </div>
-        <img
-          src="@/assets/images/dropdown-arrow.svg"
-          alt="dropdown_arrow"
-          width="30"
-          height="30"
-          class="invert"
-        />
-      </RouterLink>
-
-      <RouterLink
-        to="/"
-        class="flex justify-between text-white hover:text-[#b5b5b5]"
-      >
-        <div class="flex gap-2">
-          <p class="text-2xl font-castoro">Hot</p>
-          <img
-            src="@/assets/images/flame-icon.svg"
-            alt="category_icon"
-            width="20"
-            height="20"
-            class="invert mr-10 mb-1"
-          />
-        </div>
-        <img
-          src="@/assets/images/dropdown-arrow.svg"
-          alt="dropdown_arrow"
-          width="30"
-          height="30"
-          class="invert"
-        />
-      </RouterLink>
-
-      <RouterLink
-        to="/"
-        class="flex justify-between text-white hover:text-[#b5b5b5]"
-      >
-        <div class="flex gap-2">
-          <p class="text-2xl font-castoro">Categories</p>
-          <img
-            src="@/assets/images/category-icon.svg"
-            alt="category_icon"
-            width="20"
-            height="20"
-            class="invert mr-10"
-          />
-        </div>
-        <img
-          src="@/assets/images/dropdown-arrow.svg"
-          alt="dropdown_arrow"
-          width="30"
-          height="30"
-          class="invert mb-1"
-        />
-      </RouterLink>
+      <SidebarItem title="New" :icon-url="starIcon" />
+      <SidebarItem title="Hot" :icon-url="flameIcon" />
+      <SidebarItem title="Categories" :icon-url="categoryIcon" />
     </div>
   </section>
 </template>
--- FrontEnd/src/components/home/SidebarItem.vue
+++ FrontEnd/src/components/home/SidebarItem.vue
@@ -1,3 +1,31 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+import { defineProps } from "vue";
 
-<template></template>
+const props = defineProps(["title", "iconUrl"]);
+</script>
+
+<template>
+  <RouterLink
+    to="/"
+    class="flex justify-between text-white hover:text-[#b5b5b5]"
+  >
+    <div class="flex gap-2">
+      <p class="text-2xl font-castoro">{{ props.title }}</p>
+      <img
+        :src="props.iconUrl"
+        alt="item_icon"
+        width="20"
+        height="20"
+        class="invert mr-10 mb-1"
+      />
+    </div>
+    <img
+      src="@/assets/images/dropdown-arrow.svg"
+      alt="dropdown_arrow"
+      width="30"
+      height="30"
+      class="invert"
+    />
+  </RouterLink>
+</template>
