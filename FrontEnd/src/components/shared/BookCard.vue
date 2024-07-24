9f1162a16a65f3fbabb93ae1f88f34fed9eea2df
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-24 15:08:44 -0600

    feat(BookCard): implement the books demo title

--- FrontEnd/src/components/shared/BookCard.vue
+++ FrontEnd/src/components/shared/BookCard.vue
@@ -1,11 +1,14 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col p-5 w-60 h-[430px] rounded-lg bg-[#0A0C15]">
+  <div class="flex flex-col p-5 w-60 h-[400px] rounded-lg bg-[#0A0C15]">
     <img
       src="../../assets/images/book_portrait_demo.jpeg"
       alt="book_portrait"
-      class="w-full h-64 object-cover rounded-md"
+      class="w-full h-64 object-cover rounded-md mb-4"
     />
+    <h3 class="text-white text-lg line-clamp-2 font-bold font-castoro w-full">
+      Perdí un planeta y gane un hogar
+    </h3>
   </div>
 </template>
