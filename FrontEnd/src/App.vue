399c7f3c07c0df70cd908c53d4c8b5de9066353d
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-24 12:54:11 -0600

    feat(Navbar): implement the complete Navbar component

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,47 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import { SignedIn, SignedOut, UserButton, ClerkLoaded } from "vue-clerk";
+import { RouterView } from "vue-router";
+import { ClerkLoaded } from "vue-clerk";
+import Navbar from "./components/shared/Navbar.vue";
 </script>
 
 <template>
   <section class="min-h-screen overflow-hidden">
-    <header
-      class="relative bg-[#060815] px-36 z-50 border-b-[0.5px] border-[#a5fe52]"
-    >
-      <div class="flex justify-between items-center h-20 w-full">
-        <div @click="$router.push('/')" class="cursor-pointer">
-          <img
-            src="./assets/images/inkradar-white-logo.svg"
-            alt="logo"
-            width="180"
-          />
-        </div>
-
-        <nav class="flex items-center gap-10 text-xl font-semibold">
-          <SignedOut>
-            <RouterLink
-              to="/sign-in"
-              class="font-castoro text-[22px] text-white hover:text-[#b5b5b5]"
-              >SignIn</RouterLink
-            >
-            <RouterLink
-              to="/sign-up"
-              class="font-castoro text-[22px] text-white hover:text-[#b5b5b5]"
-              >SignUp</RouterLink
-            >
-          </SignedOut>
-
-          <SignedIn>
-            <RouterLink
-              to="/profile"
-              class="font-castoro text-[22px] text-white hover:text-[#b5b5b5]"
-              >Profile</RouterLink
-            >
-            <UserButton after-sign-out-url="/sign-in" />
-          </SignedIn>
-        </nav>
-      </div>
-    </header>
+    <Navbar />
 
     <main class="flex items-center justify-center min-h-[calc(100vh-80px)]">
       <ClerkLoaded>
--- FrontEnd/src/components/shared/Navbar.vue
+++ FrontEnd/src/components/shared/Navbar.vue
@@ -1,3 +1,44 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+import { SignedIn, SignedOut, UserButton } from "vue-clerk";
+</script>
 
-<template></template>
+<template>
+  <header
+    class="relative bg-[#060815] px-36 z-50 border-b-[0.5px] border-[#a5fe52]"
+  >
+    <div class="flex justify-between items-center h-20 w-full">
+      <div @click="$router.push('/')" class="cursor-pointer">
+        <img
+          src="@/assets/images/inkradar-white-logo.svg"
+          alt="logo"
+          width="180"
+        />
+      </div>
+
+      <nav class="flex items-center gap-10 text-xl font-semibold">
+        <SignedOut>
+          <RouterLink
+            to="/sign-in"
+            class="font-castoro text-[22px] text-white hover:text-[#b5b5b5]"
+            >SignIn</RouterLink
+          >
+          <RouterLink
+            to="/sign-up"
+            class="font-castoro text-[22px] text-white hover:text-[#b5b5b5]"
+            >SignUp</RouterLink
+          >
+        </SignedOut>
+
+        <SignedIn>
+          <RouterLink
+            to="/profile"
+            class="font-castoro text-[22px] text-white hover:text-[#b5b5b5]"
+            >Profile</RouterLink
+          >
+          <UserButton after-sign-out-url="/sign-in" />
+        </SignedIn>
+      </nav>
+    </div>
+  </header>
+</template>
