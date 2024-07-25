1f8186ff9666f3f6f60e8809e6b36b54be3a1891
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-24 20:54:47 -0600

    implement many fixes for the book cards containers along add the book card category display

--- FrontEnd/src/components/profile/ProfileCategory.vue
+++ FrontEnd/src/components/profile/ProfileCategory.vue
@@ -8,9 +8,11 @@ const cards = [1, 2, 3, 4, 5, 6, 7];
 </script>
 
 <template>
-  <article class="flex flex-col w-full h-auto">
-    <h1 class="text-white text-4xl font-castoro">{{ props.title }}:</h1>
-    <div class="flex flex-wrap justify-between w-full h-auto">
+  <article class="flex flex-col w-full h-auto pb-5 border-b border-[#B0FC63]">
+    <h1 class="text-white text-4xl font-castoro mb-7">{{ props.title }}:</h1>
+    <div
+      class="grid max-lg:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-7 place-items-center w-full h-auto"
+    >
       <BookCard v-for="(card, index) in cards" :key="index" />
     </div>
   </article>
--- FrontEnd/src/components/shared/BookCard.vue
+++ FrontEnd/src/components/shared/BookCard.vue
@@ -1,19 +1,22 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col p-5 w-60 h-[400px] rounded-lg bg-[#0A0C15]">
+  <div class="flex flex-col px-5 py-4 w-60 h-[400px] rounded-lg bg-[#0A0C15]">
     <img
       src="../../assets/images/book_portrait_demo.jpeg"
       alt="book_portrait"
-      class="w-full h-64 object-cover rounded-md mb-4"
+      class="w-full h-60 object-cover rounded-md mb-4"
     />
     <h3
       class="text-white text-lg line-clamp-2 font-bold font-castoro w-full mb-3"
     >
       Perdí un planeta y gane un hogar
     </h3>
-    <p class="text-white text-xs font-montserrat w-full line-clamp-1">
+    <p class="text-white text-xs font-montserrat w-full line-clamp-1 mb-2">
       Una novela de Kate Johnson
     </p>
+    <h5 class="w-full line-clamp-1 text-[#35c068] font-montserrat text-sm">
+      Fantasy
+    </h5>
   </div>
 </template>
