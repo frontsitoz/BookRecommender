205b3cf9cd576e76cd836a0927233bafeb2b5ce4
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-28 22:26:58 -0600

    implement the books dummy data dynamic display within the ProfileDetails and ProfileCategory components based off the new isBookmarked, isFavorite or isRead books properties

--- FrontEnd/src/components/profile/ProfileCategory.vue
+++ FrontEnd/src/components/profile/ProfileCategory.vue
@@ -1,18 +1,36 @@
 <script setup>
 import { defineProps } from "vue";
 import BookCard from "../shared/BookCard.vue";
-import { books } from "@/constants";
+const { type, books } = defineProps(["type", "books"]);
 
-const props = defineProps(["title"]);
+const bookmarkedBooks = books.filter((book) => book.isBookmarked);
+const favoriteBooks = books.filter((book) => book.isFavorite);
+const booksRead = books.filter((book) => book.isRead);
 </script>
 
 <template>
   <article class="flex flex-col w-full h-auto pb-5 border-b border-[#B0FC63]">
-    <h1 class="text-white text-4xl font-castoro mb-7">{{ props.title }}:</h1>
+    <h1 class="text-white text-4xl font-castoro mb-7">
+      {{
+        type === "bookmarked"
+          ? "Bookmarked"
+          : type === "favorites"
+          ? "Favorites"
+          : "Books Read"
+      }}:
+    </h1>
     <div
       class="grid max-lg:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-7 place-items-center w-full h-auto"
     >
-      <BookCard v-for="book in books" :key="book.id" :book="book" />
+      <BookCard
+        v-for="book in type === 'bookmarked'
+          ? bookmarkedBooks
+          : type === 'favorites'
+          ? favoriteBooks
+          : booksRead"
+        :key="book.id"
+        :book="book"
+      />
     </div>
   </article>
 </template>
