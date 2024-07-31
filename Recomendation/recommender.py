2ecb010048209998d671a5a7e0d16b14fdd5ef81
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-07-31 16:57:49 -0500

    endpointdelalgoritmo, cambios en la entidad book y se agrego entidad para la request y el mapeo de los datos

--- Recomendation/recommender.py
+++ Recomendation/recommender.py
@@ -1,33 +1,34 @@
-from schemas import Book
 from typing import List
+from schemas import Book
+
 
 def generate_recommendations(user_books: List[Book], all_books: List[Book], top_n: int = 5) -> List[Book]:
-    #creamos la lista de libros que el usuario ha leido
-    read_books = [book for book in user_books if book.read]
+    #creamos la lista de libros que el usuario ha leído
+    read_books = [book for book in user_books if book.isRead]
     
-    #identifico los libros mejor valorados que el usuario ha leído y que le interesan
-    interested_books = [book for book in read_books if book.interested]
+    #identificamos los libros mejor valorados que el usuario ha leído y que le interesan
+    interested_books = [book for book in read_books if book.isBookMarked]
     best_rated_books = sorted(interested_books, key=lambda x: x.average_rating, reverse=True)
     
-    #creo la ista donde se guardaran los libros recomendados
+    #creo la lista donde se guardaran los libros recomendados
     recommended_books = []
 
     if best_rated_books:
-        #sii el usuario ha interactuado con libros filtramos libros similares no leidos basados en el genero
+        #si el usuario ha interactuado con libros  filtramos libros similares no leidos basados en el genero
         for book in best_rated_books:
-            #filtrar los libros de allbooks que coinciden en genero y que no han sido leidos
-            similar_books = [b for b in all_books if b.gender == book.gender and not b.read]
+            #filtro los libros de all_books que coinciden en genero y que no han sido leidos
+            similar_books = [b for b in all_books if b.genre == book.genre and not b.isRead]
             for similar_book in similar_books:
-                if similar_book not in recommended_books and not similar_book.interested:
+                if similar_book not in recommended_books and not similar_book.isBookMarked:
                     recommended_books.append(similar_book)
     else:
-        #si el usuario no ha leido ningun libro  mostramos libros mejor valorados en general o populares
-        recommended_books = [book for book in all_books if not book.read and not book.not_interested]
-        # Ordenar por calificación promedio
+        #si el usuario no ha leido ningun libro, mostrar libros mejor valorados en general o populares
+        recommended_books = [book for book in all_books if not book.isRead and not book.isBookMarked]
+        #ordeno por calificación promedio
         recommended_books.sort(key=lambda x: x.average_rating, reverse=True)
         recommended_books = recommended_books[:top_n]
     
-    #ordenamos los libros recomendados por calificacion promedio y devolver los top_n libros a recomendar
+    #ordeno los libros recomendados por calificacion promedio y devolver los top_n libros a recomendar
     recommended_books.sort(key=lambda x: x.average_rating, reverse=True)
     top_recommendations = recommended_books[:top_n]
     
--- Recomendation/schemas.py
+++ Recomendation/schemas.py
@@ -1,17 +1,27 @@
 # Esquemas de datos para entrada y salida de API
 from pydantic import BaseModel, Field
-from typing import List, Optional, Dict
-#import numpy as np
+from typing import List
 
 
-
-#Clase libro
+# Clase libro
 class Book(BaseModel):
-    id: int #id 
-    title: str #titulo
-    author: Optional[List[str]] = None  # Autores del libro, opcional
-    gender :str #genero del libro
+    id: int  # ID del libro
+    title: str  #titulo del libro
+    authors: str  #autores del libro
+    description: str  #descripcion del libro
+    genre: str  #genero del libro
+    publisher: str  #editorial del libro
+    publishedDate: str  #fecha de publicacion del libro
+    imageUrl: str  #URL de la imagen del libro
+    pageCount: int  #numero de paginas del libro
+    isBookMarked: bool  #indicador si el libro está marcado
+    isRead: bool  # indicador si el libro ha sido leído
+    isReading: bool  #indicador si el libro está en lectura
+    isLike: bool  #indicador si el libro está marcado como 'me gusta'
     average_rating: float = Field(ge=0, le=5)  #calificacion promedio de 0 a 5
-    read : bool = False  #indica si el libro ha sido leido o no
-    interested : bool = False #indicador si el libro esta marcado como me interesa
-    not_interested : bool = False #indicador si el libro esta marcado como no me interesa
+
+# clase del modelado de datos para la solicitud 
+class RecommendationRequest(BaseModel):
+    user_books: List[Book]
+    all_books: List[Book]
+    top_n: int = 5
