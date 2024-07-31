f828ce4d57d7330155d2c4432796d9ae80ee455c
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-07-30 21:01:44 -0500

    ccambios en main.py y en el algoritmo

--- Recomendation/main.py
+++ Recomendation/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import requests
+from typing import Optional, Dict
 
 
 app = FastAPI()
@@ -6,6 +8,28 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "world"}
+
+
+#consumimos el endpoint del backend
+@app.get("/getAllBooks")
+def getAllBooks(query:str = Query("", description = "search Query for books"),
+                page : int = Query(0,description = "page number for pagination"),
+                size:int = Query(30,description="number of results per page")
+):
+    
+    #url del endpoint del backend
+    url = f"http://localhost:9090/api/books/searchBooks?query={query}&page={page}&size={size}"
+
+    #realizamos la solicitud get
+    response = requests.get(url)
+
+    #verificamos si la solicitud fue exitosa
+    if response.status_code == 200:
+        data  = response.json()
+        return {"data":data}
+    else:
+        return{"error": f"fail to fetch data, status code : {response.status_code}"}
+
 
 
 
--- Recomendation/recommender.py
+++ Recomendation/recommender.py
@@ -1,29 +1,34 @@
-import requests
 from schemas import Book
 from typing import List
 
-
-
 def generate_recommendations(user_books: List[Book], all_books: List[Book], top_n: int = 5) -> List[Book]:
-    #creamos la lista de libros que el usuario ha leído
+    #creamos la lista de libros que el usuario ha leido
     read_books = [book for book in user_books if book.read]
     
-    #identificamos los libros mejor valorados que el usuario ha leído y que le interesan
+    #identifico los libros mejor valorados que el usuario ha leído y que le interesan
     interested_books = [book for book in read_books if book.interested]
     best_rated_books = sorted(interested_books, key=lambda x: x.average_rating, reverse=True)
     
-    #filtramos libros similares no leídos basados en el género
+    #creo la ista donde se guardaran los libros recomendados
     recommended_books = []
-    for book in best_rated_books:
-        #filtramos los libros de all_books que coinciden en género y que no han sido leídos
-        similar_books = [b for b in all_books if b.gender == book.gender and not b.read]
-        for similar_book in similar_books:
-            if similar_book not in recommended_books and not similar_book.interested:
-                recommended_books.append(similar_book)
+
+    if best_rated_books:
+        #sii el usuario ha interactuado con libros filtramos libros similares no leidos basados en el genero
+        for book in best_rated_books:
+            #filtrar los libros de allbooks que coinciden en genero y que no han sido leidos
+            similar_books = [b for b in all_books if b.gender == book.gender and not b.read]
+            for similar_book in similar_books:
+                if similar_book not in recommended_books and not similar_book.interested:
+                    recommended_books.append(similar_book)
+    else:
+        #si el usuario no ha leido ningun libro  mostramos libros mejor valorados en general o populares
+        recommended_books = [book for book in all_books if not book.read and not book.not_interested]
+        # Ordenar por calificación promedio
+        recommended_books.sort(key=lambda x: x.average_rating, reverse=True)
+        recommended_books = recommended_books[:top_n]
     
-    #ordenar por calificación promedio y devolver los top_n libros a recomendar
+    #ordenamos los libros recomendados por calificacion promedio y devolver los top_n libros a recomendar
     recommended_books.sort(key=lambda x: x.average_rating, reverse=True)
     top_recommendations = recommended_books[:top_n]
     
     return top_recommendations
-
