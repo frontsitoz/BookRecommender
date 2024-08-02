45f6f7657518d415e7e3f18d42a9bd86d90eb815
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-08-02 17:15:11 -0500

    Control para valores null que puedan llegar en la peticion

--- Recomendation/schemas.py
+++ Recomendation/schemas.py
@@ -5,19 +5,19 @@ from typing import List, Optional
 
 # Clase libro
 class Book(BaseModel):
-    id: int  # ID del libro
+    id: Optional[int]  # ID del libro
     title: str  #titulo del libro
-    authors: str  #autores del libro
-    description: str  #descripcion del libro
+    authors: Optional[str]  #autores del libro
+    description: Optional[str]  #descripcion del libro
     genre: str  #genero del libro
-    publisher: str  #editorial del libro
-    publishedDate: str  #fecha de publicacion del libro
-    imageUrl: str  #URL de la imagen del libro
-    pageCount: int  #numero de paginas del libro
-    isBookMarked: bool  #indicador si el libro está marcado
-    isRead: bool  # indicador si el libro ha sido leído
-    isReading: bool  #indicador si el libro está en lectura
-    isLike: bool  #indicador si el libro está marcado como 'me gusta'
+    publisher: Optional[str]  #editorial del libro
+    publishedDate: Optional[str]  #fecha de publicacion del libro
+    imageUrl: Optional[str]  #URL de la imagen del libro
+    pageCount: Optional[int]  #numero de paginas del libro
+    isBookMarked: Optional[bool] = None  #indicador si el libro está marcado
+    isRead: Optional[bool] = None  # indicador si el libro ha sido leído
+    isReading: Optional[bool] = None   #indicador si el libro está en lectura
+    isLike: Optional[bool] = None  #indicador si el libro está marcado como 'me gusta'
     average_rating: float = Field(ge=0, le=5)  #calificacion promedio de 0 a 5
 
 # clase del modelado de datos para la solicitud 
